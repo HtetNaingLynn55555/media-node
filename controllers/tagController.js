a1b4b9a525445253d1a7438b0092e2f8ec7f10f1
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-02-27 00:35:14 +0630

    all tag fetching  done

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -1,6 +1,16 @@
+let DB = require('../models/tag');
+let {success} = require('../utils/Helper');
 
 let all = async(request, response, next)=>{
-
+    let tags = await DB.find();
+    if(tags)
+    {
+        success(response, 200, 'tag list fetching success', tags);
+    }
+    else
+    {
+        next(new Error('tag list fetching fail'))
+    }
 }
 
 let create = async(request, response, next)=>{
