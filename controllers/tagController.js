d80401e2616a0e5f55909fce06ddf3672b88911e
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-02-27 00:22:48 +0630

    tag controller demo done

---
+++ controllers/tagController.js
@@ -0,0 +1,28 @@
+
+let all = async(request, response, next)=>{
+
+}
+
+let create = async(request, response, next)=>{
+
+}
+
+let details = async(request, response, next)=>{
+
+}
+
+let update = async(request, response, next)=>{
+
+}
+
+let drop = async(request, response, next)=>{
+
+}
+
+module.exports = {
+    all,
+    create,
+    details,
+    update,
+    drop
+}
