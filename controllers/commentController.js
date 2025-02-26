ea0aa0d97139a5ffc4643c1df1ab1ac687314f08
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-02-27 01:34:27 +0630

    add commentcontroller demo done

---
+++ controllers/commentController.js
@@ -0,0 +1,21 @@
+
+let DB = require('../models/comment');
+let {success} = require('../utils/Helper');
+
+let add = async(request, response, next)=>{
+
+}
+
+let update = async(request, response, next)=>{
+
+}
+
+let drop = async(request, response, next)=>{
+
+}
+
+module.exports = {
+    add,
+    update,
+    drop,
+}
