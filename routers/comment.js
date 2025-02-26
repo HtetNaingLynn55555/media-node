2adaf16d61e68e5e9431d1c5a8914518cf511468
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-02-27 01:39:17 +0630

    comment route done

--- routers/comment.js
+++ routers/comment.js
@@ -3,3 +3,8 @@ let commentController = require('../controllers/commentController');
 
 router.get('/', commentController.all);
 router.post('/', commentController.create);
+router.route('/:')
+        .patch(commentController.update)
+        .delete(commentController.drop);
+
+module.exports = router;
