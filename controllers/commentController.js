c58205a2f7d7ec41b0741de4f3c38e48931bdbd9
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-02-27 20:25:14 +0630

    comment create and all done

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -3,10 +3,26 @@ let DB = require('../models/comment');
 let {success} = require('../utils/Helper');
 
 let all = async(request, response, next)=>{
-
+    let comments = await DB.find();
+    if(comments)
+    {
+        success(response, 200, 'comment fetching success', comments)
+    }
+    else
+    {
+        next(new Error('comment fetching fail'))
+    }
 }
 let create = async(request, response, next)=>{
-
+    let comment = await DB.create(request.body);
+    if(comment)
+    {
+        success(response, 201, 'comment create success', comment)
+    }
+    else
+    {
+        next(new Error('comment create fail'))
+    }
 }
 
 let update = async(request, response, next)=>{
