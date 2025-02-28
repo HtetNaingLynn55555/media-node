5374efa54407b094d8f7dc5956507c6193d2a0d3
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-02-28 09:50:20 +0630

    post model redefine and validationSchema change for post

--- models/post.js
+++ models/post.js
@@ -5,6 +5,8 @@ let postSchema = new Schema({
     user_id : {type : Schema.Types.ObjectId, ref : 'user', required : true},
     category_id: {type: Schema.Types.ObjectId, ref: 'category', required: true},
     title : {type : String, required : true},
+    tag : {type:Schema.Types.ObjectId, ref:'tag', required: true},
+    like : {type: Number, default : 0},
     content : {type : String, required: true},
     image : {type : String},
     created_at : {type :Date, default : Date.now},
